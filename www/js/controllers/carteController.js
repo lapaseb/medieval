20d9b0238c44bf8728cae12c48a97ca7e3b1bfce
lapaseb <sebastien.lapaire@divtec.ch>
2016-10-12 15:20:56 +0200

    Nouvelle carte offline + ajout des maxbounds
    Ajout d'une nouvelle carte plus détaillée avec un niveau de zoom
    amélioré ainsi que la gestion des maxbounds et du niveau de zoom

--- www/js/controllers/carteController.js
+++ www/js/controllers/carteController.js
@@ -3,23 +3,35 @@
 angular.module('starter.controllers')
 .controller('CarteCtrl', ['$scope', '$http','$rootScope',
     function($scope, $http, $rootScope) {
-		var map = L.map('mapid').setView([47.364965, 7.154498], 16);
 
-		  L.tileLayer('../../data/img/MapQuest/{z}/{x}/{y}.png', {
-			  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-		  }).addTo(map);
+    var southWest = L.latLng(47.369743926768784, 7.174824539917747),
+    northEast = L.latLng(47.360589810163582, 7.1379860116837257),
+    bounds = L.latLngBounds(southWest, northEast);
 
-		  // Cordier
-		  L.marker([47.365714, 7.155803]).addTo(map).bindPopup("Cordier");
+    var map = L.map('mapid', {
+        center: [47.364965, 7.154498],
+        zoom: 18,
+        minZoom: 15,
+        maxNativeZoom: 20,
+        maxBounds: bounds
+    });
 
-		  // Minéraux
-		  L.marker([47.365660, 7.155120]).addTo(map).bindPopup("Minéraux");
+	  L.tileLayer('../../data/img/MapQuest/{z}/{x}/{y}.png', {
+		  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	  }).addTo(map);
 
-		  // Tailleur d'ardoises
-		  L.marker([47.365002, 7.155689]).addTo(map).bindPopup("Tailleur d'ardoises");
 
-		  // Epices
-		  L.marker([47.364907, 7.154937]).addTo(map).bindPopup("Epices");
+	  // Cordier
+	  L.marker([47.365714, 7.155803]).addTo(map).bindPopup("Cordier");
+
+	  // Minéraux
+	  L.marker([47.365660, 7.155120]).addTo(map).bindPopup("Minéraux");
+
+	  // Tailleur d'ardoises
+	  L.marker([47.365002, 7.155689]).addTo(map).bindPopup("Tailleur d'ardoises");
+
+	  // Epices
+	  L.marker([47.364907, 7.154937]).addTo(map).bindPopup("Epices");
 
     }
 ]);
