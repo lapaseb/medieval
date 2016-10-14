3ea752734ab3f583af0f8075ed9e03eff22ac3db
Dan "Des Neiges" Goudron <dan.goudron@divtec.ch>
2016-10-14 10:13:36 +0200

    Ajout de valeurs d'exemple
    Ajout d'établissement avec des valeurs d'exemple
    Ajout d'évenement avec des valeurs d'exemple

--- www/js/controllers/programmesController.js
+++ www/js/controllers/programmesController.js
@@ -1,14 +1,15 @@
 angular.module('starter.controllers')
 .controller('ProgrammesCtrl', ['$scope', '$http','$rootScope', 'programmesService',
     function($scope, $http, $rootScope, programmesService) {
-       
+
 		programmesService.get(function (data) {
 			$scope.programmesRow = data;
 			$scope.programmes = [];
 			for (var i = 0; i < $scope.programmesRow.events.length; i++) {
 				$scope.programmes[i] = {
-					id: i, 
+					id: i,
 					name: $scope.programmesRow.events[i].name,
+          description: $scope.programmesRow.events[i].description,
 					start: $scope.programmesRow.events[i].start,
 					end: $scope.programmesRow.events[i].end
 				};
