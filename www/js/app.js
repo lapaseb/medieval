ba95efbde72f7609520065a2a7dfc2689e583cc4
Sébastien <sebastien.brainlolz@gmail.com>
2016-10-14 18:06:39 +0200

    Ajout de la page sponsors

--- www/js/app.js
+++ www/js/app.js
@@ -125,6 +125,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       }
     }
   })
+  .state('app.sponsors', {
+    url: '/sponsors',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/sponsors.html',
+        controller: 'SponsorsCtrl'
+      }
+    }
+  })
 
   // if none of the above states are matched, use this as the fallback
   ;$urlRouterProvider.otherwise('/app/accueil');
