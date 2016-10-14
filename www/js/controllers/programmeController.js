7739ab9cc95d3ff57df345cbf18dbe4f4ed52c2c
Sébastien <sebastien.brainlolz@gmail.com>
2016-10-14 23:17:02 +0200

    Adaptation du controller au nouveau JSON

--- www/js/controllers/programmeController.js
+++ www/js/controllers/programmeController.js
@@ -1,19 +1,20 @@
 angular.module('starter.controllers')
 .controller('ProgrammeCtrl', ['$scope', '$http','$rootScope', '$stateParams', 'programmesService',
     function($scope, $http, $rootScope, $stateParams, programmesService) {
-       
+
     	var progid = $stateParams.programmeId;
 
 		programmesService.get(function (data) {
 			$scope.programmesRow = data;
 			$scope.programme = [];
-			for (var i = 0; i < $scope.programmesRow.events.length; i++) {
+			for (var i = 0; i < $scope.programmesRow.length; i++) {
 				if(i == progid) {
 					$scope.programme = {
-						id: i, 
-						name: $scope.programmesRow.events[i].name,
-						start: $scope.programmesRow.events[i].start,
-						end: $scope.programmesRow.events[i].end
+						id: i,
+						name: $scope.programmesRow[i].name,
+            description: $scope.programmesRow[i].description,
+						start: $scope.programmesRow[i].start,
+						end: $scope.programmesRow[i].end
 					};
 				}
 			}
