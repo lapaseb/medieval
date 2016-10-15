edb38c92b19831882c75889b373d519066fd7633
Sébastien <sebastien.brainlolz@gmail.com>
2016-10-15 17:51:25 +0200

    Suppression des marqueurs personnalisés

--- www/js/controllers/carteController.js
+++ www/js/controllers/carteController.js
@@ -62,91 +62,16 @@ angular.module('starter.controllers')
 
     map.addControl(new ourCustomControl());
 
-
-
-    var zoo = L.icon({
-        iconUrl: 'data/img/icons/zoo.png',
-        iconSize:     [32, 37], // size of the icon
-        iconAnchor:   [16, 37], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-
-    var number1 = L.icon({
-        iconUrl: 'data/img/icons/number_1.png',
-        iconSize:     [32, 37], // size of the icon
-        iconAnchor:   [16, 37], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-
-    var number2 = L.icon({
-        iconUrl: 'data/img/icons/number_2.png',
-        iconSize:     [32, 37], // size of the icon
-        iconAnchor:   [16, 37], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-
-    var number3 = L.icon({
-        iconUrl: 'data/img/icons/number_3.png',
-        iconSize:     [32, 37], // size of the icon
-        iconAnchor:   [16, 37], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-
-    var camping = L.icon({
-        iconUrl: 'data/img/icons/campingtents.png',
-        iconSize:     [32, 37], // size of the icon
-        iconAnchor:   [16, 37], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-
-    var bank = L.icon({
-        iconUrl: 'data/img/icons/bank_icon.png',
-        iconSize:     [32, 37], // size of the icon
-        iconAnchor:   [16, 37], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-
-    var toilets = L.icon({
-        iconUrl: 'data/img/icons/toilets.png',
-        iconSize:     [32, 37], // size of the icon
-        iconAnchor:   [16, 37], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-
-    var firstaid = L.icon({
-        iconUrl: 'data/img/icons/firstaid.png',
-        iconSize:     [32, 37], // size of the icon
-        iconAnchor:   [16, 37], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-
-    var parking = L.icon({
-        iconUrl: 'data/img/icons/Parking.png',
-        iconSize:     [32, 37], // size of the icon
-        iconAnchor:   [16, 37], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-
-    var entrance = L.icon({
-        iconUrl: 'data/img/icons/entrance.png',
-        iconSize:     [32, 37], // size of the icon
-        iconAnchor:   [16, 37], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-
-
-	  L.marker([47.365714, 7.155803], {icon: zoo}).addTo(map);
-	  L.marker([47.365660, 7.155120], {icon: number1}).addTo(map);
-	  L.marker([47.365002, 7.155689], {icon: number2}).addTo(map);
-	  L.marker([47.364907, 7.154937], {icon: firstaid}).addTo(map);
-    L.marker([47.364814, 7.154437], {icon: camping}).addTo(map);
-    L.marker([47.366814, 7.152437], {icon: bank}).addTo(map);
-    L.marker([47.366514, 7.152237], {icon: toilets}).addTo(map);
-    L.marker([47.366614, 7.152337], {icon: parking}).addTo(map);
-    L.marker([47.364424, 7.153161], {icon: entrance}).addTo(map);
-    L.marker([47.365969, 7.155872], {icon: entrance}).addTo(map);
-
-
+	  L.marker([47.365714, 7.155803]).addTo(map);
+	  L.marker([47.365660, 7.155120]).addTo(map);
+	  L.marker([47.365002, 7.155689]).addTo(map);
+	  L.marker([47.364907, 7.154937]).addTo(map);
+    L.marker([47.364814, 7.154437]).addTo(map);
+    L.marker([47.366814, 7.152437]).addTo(map);
+    L.marker([47.366514, 7.152237]).addTo(map);
+    L.marker([47.366614, 7.152337]).addTo(map);
+    L.marker([47.364424, 7.153161]).addTo(map);
+    L.marker([47.365969, 7.155872]).addTo(map);
 
 
     }
