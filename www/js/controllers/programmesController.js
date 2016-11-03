15c4e1669142ab849107cdd8830132aab3ed3ce5
Zeldroxe <romainfranc@msn.com>
2016-11-03 16:22:21 +0100

    Debug

--- www/js/controllers/programmesController.js
+++ www/js/controllers/programmesController.js
@@ -1,6 +1,13 @@
 angular.module('starter.controllers')
 .controller('ProgrammesCtrl', ['$scope', '$http','$rootScope', 'programmesService',
     function($scope, $http, $rootScope, programmesService) {
+
+    	$rootScope.$on("$ionicView.enter", function(scopes, states) {
+			if (states.stateName == "app.programmes") {
+				$scope.doRefresh();
+			}
+		});
+
 		$scope.doRefresh = function() {
 			programmesService.get(function (data) {
 				$scope.programmesRow = data;
@@ -8,8 +15,8 @@ angular.module('starter.controllers')
 				for (var i = 0; i < $scope.programmesRow.length; i++) {
 					$scope.programmes[i] = {
 						id: i,
-						name: $scope.programmesRow[i].name,
-	          			description: $scope.programmesRow[i].description,
+						name: $scope.programmesRow[i]["name_" + window.localStorage.getItem("lang")],
+	          			description: $scope.programmesRow[i]["description_" + window.localStorage.getItem("lang")],
 						start: $scope.programmesRow[i].start,
 						end: $scope.programmesRow[i].end
 					};
@@ -19,5 +26,6 @@ angular.module('starter.controllers')
 	  	};
 
 	  	$scope.doRefresh();
+
     }
 ]);
