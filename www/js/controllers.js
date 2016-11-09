639516cbeadf27b632eee2d4f4b357c5724a0362
Mathis Schaller <mathis.schaller@divtec.ch>
2016-11-09 14:55:25 +0100

    modifications legeres
    asd

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,11 @@
 angular.module('starter.controllers', [])
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $rootScope) {
 	//Ouvreur de filtres
-	$rootScope.openFilter   = function(viewName){console.log('controller.js open');openFilter(viewName);};
+	$rootScope.openFilter   = function(viewName){openFilter(viewName);};
 	//Fermeur de filtres
-	$rootScope.closeFilter  = function(viewName){console.log('controller.js close');closeFilter(viewName);};
+	$rootScope.closeFilter  = function(viewName){closeFilter(viewName);};
 	//Accesseur de filtres
-	$rootScope.getFilter    = function(viewName){console.log('controller.js get');return getFilter(viewName);};
+	$rootScope.getFilter    = function(viewName){return getFilter(viewName);};
 	//Changeur de filtre
-	$rootScope.changeFilter = function(viewName, filter){console.log('controller.js change');changeFilter(viewName, filter);};
+	$rootScope.changeFilter = function(viewName, filter){changeFilter(viewName, filter);};
 });
